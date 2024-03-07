70c0ef2300fa8dbcb2a82745d02d8618530cc865
Federico Paredes <fede_paredes21@hotmail.com>
2024-03-07 01:01:57 -0300

    Revert "Configuracion del archivo de propiedades. Mapeo objeto realcional y creacion de la base de datos."
    This reverts commit 29f17bbc49b9c97539da6f36465eac427966aaf0.

--- src/main/java/com/paredesfederico/entidades/Genero.java
+++ src/main/java/com/paredesfederico/entidades/Genero.java
@@ -1,32 +1,14 @@
 package com.paredesfederico.entidades;
 
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-@Entity
-@Table(name = "generos")
 public class Genero {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "gen_id")
 	private Integer iid;
 	
-	@Column(name = "gen_nombre", nullable = false, length = 15)
 	private String nombreGenero;
 	
-	@ManyToMany(mappedBy = "generos", fetch = FetchType.EAGER)
-	private List<Pelicula> peliculas;
 	
 	
 }
