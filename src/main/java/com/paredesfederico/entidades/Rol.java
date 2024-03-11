34159a9183d7eddbf609e96bd6bd106d88fc8392
Federico Paredes <fede_paredes21@hotmail.com>
2024-03-11 01:02:05 -0300

    Se crean las entidades de usuario y rol y se mapean a la base de datos.

---
+++ src/main/java/com/paredesfederico/entidades/Rol.java
@@ -0,0 +1,28 @@
+package com.paredesfederico.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+
+public class Rol {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "rol_id")
+	private Long id;
+	
+	@Column(name = "rol_nombre_rol", nullable = false)
+	private String nombreRol;
+
+	
+	
+	
+}
